71372b4b28357de88f7e69d90ae36ca49815a777
Victoria Lozko <v.lozko@soft-industry.com>
2017-10-03 19:02:31 +0300

    fixed commit

--- src/main/java/myprojects/dz2/pages/DashboardPage.java
+++ src/main/java/myprojects/dz2/pages/DashboardPage.java
@@ -57,48 +57,73 @@ public class DashboardPage {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.elementToBeClickable(logoutImage));
 
-        WebElement menuElement = driver.findElement(menuLocator);
-        List<WebElement> menuItems = menuElement.findElements(menuItemsLocator);
-        WebElement item;
+        WebElement element = driver.findElement(By.id("tab-AdminDashboard"));
+        printInfo(element);
+        element = driver.findElement(By.id("subtab-AdminParentOrders"));
+        printInfo(element);
+        //element = driver.findElement(By.id("subtab-AdminCatalog"));
+        //printInfo(element);
+        element = driver.findElement(By.id("subtab-AdminParentCustomer"));
+        printInfo(element);
+        element = driver.findElement(By.id("subtab-AdminParentCustomerThreads"));
+        printInfo(element);
+        element = driver.findElement(By.id("subtab-AdminStats"));
+        printInfo(element);
+        //element = driver.findElement(By.id("subtab-AdminParentModulesSf"));
+        //printInfo(element);
+        element = driver.findElement(By.id("subtab-AdminParentThemes"));
+        printInfo(element);
+        element = driver.findElement(By.id("subtab-AdminParentShipping"));
+        printInfo(element);
+        element = driver.findElement(By.id("subtab-AdminParentPayment"));
+        printInfo(element);
+        element = driver.findElement(By.id("subtab-AdminInternational"));
+        printInfo(element);
+        element = driver.findElement(By.id("subtab-ShopParameters"));
+        printInfo(element);
+        element = driver.findElement(By.id("subtab-AdminAdvancedParameters"));
+        printInfo(element);
+
+        return;
+    }
+
+    public void printInfo(WebElement item){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
         String title;
         String newTitle;
-
-        for (int i = 0; i < menuItems.size(); i++){
-            item = menuItems.get(i);
-            String id = item.getAttribute("id").toString();
-            if (!id.equals("tab-SELL") && !id.equals("tab-IMPROVE") && !id.equals("tab-CONFIGURE")){
+        String id = item.getAttribute("id").toString();
+        if (!id.equals("tab-SELL") && !id.equals("tab-IMPROVE") && !id.equals("tab-CONFIGURE")){
 //Нажатие на каждый видимый пункт главного меню
-                item.click();
+            item.click();
 
-                if (id.equals("subtab-AdminCatalog") || id.equals("subtab-AdminParentModulesSf")) {
-                    wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#quick-access")));
-                }else{
-                    wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#quick_select")));
-                }
-// Вывод заголовка
-                if (id.equals("subtab-AdminCatalog") || id.equals("subtab-AdminParentModulesSf")) {
-                    title = driver.findElement(titleLocator).getText().toString();
-                }else{
-                    title = driver.findElement(page_titleLocator).getText().toString();
-                }
-                System.out.println(title);
-// Обновление страницы
-                 driver.navigate().refresh();
-
-                if (id.equals("subtab-AdminCatalog") || id.equals("subtab-AdminParentModulesSf")) {
-                    newTitle = driver.findElement(titleLocator).getText().toString();
-                }else{
-                    newTitle = driver.findElement(page_titleLocator).getText().toString();
-                }
-//Проверка, что пользователь остается в том же разделе после перезагрузки страницы
-                if (title.equals(newTitle)){
-                    System.out.println("После перезагрузки раздел не поменялся");
-                }else {
-                    System.out.println("После перезагрузки раздел поменялся");
-                }
+            if (id.equals("subtab-AdminCatalog") || id.equals("subtab-AdminParentModulesSf")) {
+                wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#quick-access")));
+            }else{
+                wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#quick_select")));
             }
+// Вывод заголовка
+            if (id.equals("subtab-AdminCatalog") || id.equals("subtab-AdminParentModulesSf")) {
+                title = driver.findElement(titleLocator).getText().toString();
+            }else{
+                title = driver.findElement(page_titleLocator).getText().toString();
+            }
+            System.out.println(title);
+// Обновление страницы
+            driver.navigate().refresh();
 
+            if (id.equals("subtab-AdminCatalog") || id.equals("subtab-AdminParentModulesSf")) {
+                newTitle = driver.findElement(titleLocator).getText().toString();
+            }else{
+                newTitle = driver.findElement(page_titleLocator).getText().toString();
+            }
+//Проверка, что пользователь остается в том же разделе после перезагрузки страницы
+            if (title.equals(newTitle)){
+                System.out.println("После перезагрузки раздел не поменялся");
+            }else {
+                System.out.println("После перезагрузки раздел поменялся");
+            }
         }
 
+        return;
     }
 }
